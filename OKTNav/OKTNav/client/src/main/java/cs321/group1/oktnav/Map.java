dd6640aeb5ae576def84431e9bea0780be31ee5b
ac0146 <41705237+ac0146@users.noreply.github.com>
2020-11-25 16:16:56 -0600

    Add files via upload
    Added changes to Navigator and Map classes to be reviewed by the group. These are subject to change, as we get further into the Dijkstra/A* algorithm discussion.

--- OKTNav/OKTNav/client/src/main/java/cs321/group1/oktnav/Map.java
+++ OKTNav/OKTNav/client/src/main/java/cs321/group1/oktnav/Map.java
@@ -1,14 +1,38 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
 package cs321.group1.oktnav;
 
+import java.util.ArrayList;
 /**
- *
- * @author Tobin
+ * Class implementation of the Map class
+ * This class contains the list of locations that exist in the OKT building
+ * @author Drew
  */
 public class Map {
+    // Member variable; stores all locations declared
+    private ArrayList<Location> allLocations;
+    // Member variable; stores the path from starting point to end point
+    private ArrayList<Location> path;
+    /**
+     * Constructor for the Map class
+     * @param locations - list of locations that exist in the OKT building
+     */
+    public Map(ArrayList<Location> locations){
+        allLocations = new ArrayList<Location>(locations);
+    }
+     /**
+      * Function that sets the path from the starting point to the end point, selected by the user
+      * @param path - the path from the starting point to the end point
+      */
+    public void setPath(ArrayList<Location> path){
+        // Setting the parameter path as the object's path
+        this.path = new ArrayList<Location>(path);
+    }
     
+    /**
+     * Function that returns the calculated path from the starting point to the end point
+     * @return the path from the starting point to the end point
+     */
+    public ArrayList<Location> getPath(){
+        // Returning the object's path where necessary
+        return new ArrayList<Location>(path);
+    }
 }
