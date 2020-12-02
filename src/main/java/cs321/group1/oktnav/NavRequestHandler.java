dc0b321f24ace1259fd98cacb8870bdbdb96ccc6
Areeb Mohammed <areebmohammed11@gmail.com>
2020-12-02 02:15:46 -0600

    Inserted JSONObject example

--- src/main/java/cs321/group1/oktnav/NavRequestHandler.java
+++ src/main/java/cs321/group1/oktnav/NavRequestHandler.java
@@ -4,6 +4,7 @@ import com.sun.net.httpserver.HttpExchange;
 import com.sun.net.httpserver.HttpHandler;
 import java.io.IOException;
 import java.io.OutputStream;
+import org.json.JSONObject;
 
 /**
  *
@@ -20,8 +21,11 @@ public class NavRequestHandler implements HttpHandler {
         String preference = params[2].replace("pref=", "");
         
         System.out.println(navFrom + " to " + navTo + " with preference of " + preference);
+
+        JSONObject replyBuilder = new JSONObject();        
+        replyBuilder.put("test", 2013);
         
-        String reply = "{\"test\":2013}";
+        String reply = replyBuilder.toString();
         
         exchange.sendResponseHeaders(200, reply.getBytes().length);
         OutputStream response = exchange.getResponseBody();
