2e86be118e7a96528c65dcf3bc40652bf56224c8
Areeb Mohammed <areebmohammed11@gmail.com>
2020-12-03 21:01:05 -0600

    Documentation for ExitHandler and PathHandler

--- OKTNav/src/main/java/cs321/group1/oktnav/ExitHandler.java
+++ OKTNav/src/main/java/cs321/group1/oktnav/ExitHandler.java
@@ -4,19 +4,45 @@ import com.sun.net.httpserver.HttpExchange;
 import com.sun.net.httpserver.HttpHandler;
 import com.sun.net.httpserver.HttpServer;
 import java.io.IOException;
+import java.io.OutputStream;
 
 /**
- *
+ * A class that shuts down the application upon receiving an exit message.
  * @author Areeb
  */
 public class ExitHandler implements HttpHandler {
+    // Reference to the server instance running the app.
     private HttpServer server;
+    
+    /**
+     * Constructor for the ExitHandler.
+     * @param server the server instance running the app.
+     */
     public ExitHandler(HttpServer server) {
+        // Store reference to server
         this.server = server;
     }    
 
+    /**
+     * Handles the exchange. Replies to the exchange with a confirmation before
+     * shutting down the application.
+     * @param exchange the HttpExchange between the app and front end.
+     * @throws IOException
+     */
     @Override
     public void handle(HttpExchange exchange) throws IOException {
+        String reply = "I am Inevitable";
+        
+        // Prepares response to the GUI
+        exchange.sendResponseHeaders(200, reply.getBytes().length);
+        OutputStream response = exchange.getResponseBody();
+        
+        // Writes from the resource stream to the response stream
+        response.write(reply.getBytes());
+        response.flush();
+        response.close();
+        
+        // Shutdown Sequence
         server.stop(0);
         System.out.println("End Game");
         System.exit(0);
--- OKTNav/src/main/java/cs321/group1/oktnav/PathHandler.java
+++ OKTNav/src/main/java/cs321/group1/oktnav/PathHandler.java
@@ -7,28 +7,39 @@ import java.io.InputStream;
 import java.io.OutputStream;
 
 /**
- *
- * @author Areeb
+ * A class that serves files in the resource directory to the GUI.
+ * @author Areeb and Tobin
  */
 public class PathHandler implements HttpHandler {
     private final String path;
     private final String contentType;
 
+    /**
+     * Constructor for the PathHandler class.
+     * @param path path to the file from the resource directory.
+     * @param contentType HTML identifier for the content type.
+     */
     public PathHandler(String path, String contentType) {
         this.path = path;
         this.contentType = contentType;
     }
 
+    /**
+     * Handles the exchange. Replies with 
+     * @param exchange
+     * @throws IOException
+     */
     @Override
     public void handle(HttpExchange exchange) throws IOException {
-        System.out.println(exchange.getRemoteAddress());
-
+        // Loads the resources as a stream
         InputStream page = ClassLoader.getSystemResourceAsStream(path);
-
+        
+        // Prepares the response to the GUI
         exchange.getResponseHeaders().set("Content-Type", contentType + ";");
         exchange.sendResponseHeaders(200, page.available());
         OutputStream response = exchange.getResponseBody();
-
+        
+        // Writes from the resource stream to the response stream
         response.write(page.readAllBytes());
         response.flush();
         response.close();
